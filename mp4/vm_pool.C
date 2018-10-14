4b1dfe5f855a5526f0f04432b2f87b121ad286af
Anjali Chadha <anjali.chadha229@gmail.com>
2018-10-14 12:40:35 -0600

    VMPool: Implemetation of Allocate()

--- mp4/vm_pool.C
+++ mp4/vm_pool.C
@@ -48,22 +48,45 @@ VMPool::VMPool(unsigned long  _base_address,
                unsigned long  _size,
                ContFramePool *_frame_pool,
                PageTable     *_page_table) {
-    assert(false);
+    base_address = _base_address;
+    size = _size;
+    frame_pool = _frame_pool;
+    page_table = _page_table;
+    page_table->register_pool(this);
     Console::puts("Constructed VMPool object.\n");
 }
 
+
+//TODO: Perfrom roundUp operation of the input size
 unsigned long VMPool::allocate(unsigned long _size) {
-    assert(false);
+
+    assert(_size > 0);
+    if(regionsCount >= VMPool::MAX_REGIONS) {
+         Console::puts("Cannot allocate more regions for the current memory pool!\n");
+	 for(;;);
+    }
+    unsigned long start_address;
+    if(regionsCount <= 0) {
+        start_address = base_address;	
+    } else {
+	unsigned long last_start_address = regions[regionsCount-1].start_address;
+	unsigned long size = regions[regionsCount - 1].size;
+        start_address = last_start_address + size;
+}
+    regions[regionsCount].start_address = start_address;
+    regions[regionsCount].size = size;
+    regionsCount++;
     Console::puts("Allocated region of memory.\n");
+    return start_address;
 }
 
 void VMPool::release(unsigned long _start_address) {
-    assert(false);
+
     Console::puts("Released region of memory.\n");
 }
 
 bool VMPool::is_legitimate(unsigned long _address) {
-    assert(false);
+ 
     Console::puts("Checked whether address is part of an allocated region.\n");
 }
 
