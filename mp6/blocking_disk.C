cb1ca847591f8fb2d4d306c9af82919242dfc9b0
Anjali Chadha <anjali.chadha229@gmail.com>
2018-11-14 18:12:14 -0600

    blocked disk queue implementation

--- mp6/blocking_disk.C
+++ mp6/blocking_disk.C
@@ -12,7 +12,7 @@
 /* DEFINES */
 /*--------------------------------------------------------------------------*/
 
-    /* -- (none) -- */
+/* -- (none) -- */
 
 /*--------------------------------------------------------------------------*/
 /* INCLUDES */
@@ -29,29 +29,85 @@ extern Scheduler* SYSTEM_SCHEDULER;
 /* CONSTRUCTOR */
 /*--------------------------------------------------------------------------*/
 
-BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size) 
-  : SimpleDisk(_disk_id, _size) {
+BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size)
+        : SimpleDisk(_disk_id, _size) {
+    head = NULL;
+    tail = NULL;
 }
 
 /*--------------------------------------------------------------------------*/
 /* SIMPLE_DISK FUNCTIONS */
 /*--------------------------------------------------------------------------*/
 
-
-void BlockingDisk::wait_until_ready() {
-   while(!is_ready()) {
-       SYSTEM_SCHEDULER->resume(Thread::CurrentThread());
-       SYSTEM_SCHEDULER->yield();
-   }
-}
 void BlockingDisk::read(unsigned long _block_no, unsigned char * _buf) {
-  // -- REPLACE THIS!!!
-  SimpleDisk::read(_block_no, _buf);
 
+    issue_operation(READ, _block_no);
+    Thread* curr_thrd = Thread::CurrentThread();
+    while (!is_ready()) {
+        addToBlockedThreadsQueue(curr_thrd);
+        SYSTEM_SCHEDULER->yield();
+    }
+
+    removeReadyThreadFromBlockedQueue(curr_thrd);
+    /* read data from port */
+    int i;
+    unsigned short tmpw;
+    for (i = 0; i < 256; i++) {
+        tmpw = Machine::inportw(0x1F0);
+        _buf[i*2]   = (unsigned char)tmpw;
+        _buf[i*2+1] = (unsigned char)(tmpw >> 8);
+    }
 }
-
 
 void BlockingDisk::write(unsigned long _block_no, unsigned char * _buf) {
-  // -- REPLACE THIS!!!
-  SimpleDisk::write(_block_no, _buf);
+    issue_operation(READ, _block_no);
+    Thread* curr_thrd = Thread::CurrentThread();
+    while (!is_ready()) {
+        addToBlockedThreadsQueue(curr_thrd);
+        SYSTEM_SCHEDULER->yield();
+    }
+
+    removeReadyThreadFromBlockedQueue(curr_thrd);
+    /* write data to port */
+    int i;
+    unsigned short tmpw;
+    for (i = 0; i < 256; i++) {
+        tmpw = _buf[2*i] | (_buf[2*i+1] << 8);
+        Machine::outportw(0x1F0, tmpw);
+    }
+
+}
+
+void BlockingDisk::addToBlockedThreadsQueue(Thread* thread) {
+    BlockedQNode* nw_thrd = new BlockedQNode();
+    nw_thrd->tcb = thread;
+    nw_thrd->next = NULL;
+    if(head == NULL) {
+        head = nw_thrd;
+        tail = nw_thrd;
+    } else {
+        tail->next = nw_thrd;
+        tail = nw_thrd;
+    }
+}
+
+void BlockingDisk::removeReadyThreadFromBlockedQueue(Thread* thread) {
+    if(head == NULL) return;
+    BlockedQNode* prev = NULL;
+    BlockedQNode* curr = head;
+    while(curr != NULL) {
+        //Remove this thread from the queue
+        if(curr->tcb == thread) {
+            if(prev == NULL) {
+                head = head->next;
+                return;
+            } else {
+                prev->next = curr->next;
+                return;
+            }
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+    return;
 }
